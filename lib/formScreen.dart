259396ed402dfd88459e70b6a2435cf67cf482d5
alvido97 <hashanforinsta1@gmail.comm>
2021-10-05 23:01:34 +0530

    1st commint

---
+++ lib/formScreen.dart
@@ -0,0 +1,282 @@
+
+
+import 'package:flutter/material.dart';
+
+class FormScreen extends StatefulWidget {
+  const FormScreen({Key? key}) : super(key: key);
+
+  @override
+  _FormScreenState createState() => _FormScreenState();
+}
+
+class _FormScreenState extends State<FormScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: Icon(Icons.arrow_back),
+        title: Text('Loan Details'),
+      ),
+      body: Container(
+        child: SingleChildScrollView(
+          child: Container(
+            padding: EdgeInsets.only(left: 10, top: 10, right: 10),
+            child: Column(
+              // mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Personal Derails',
+                  style: TextStyle(
+                      color: Colors.blueAccent, fontWeight: FontWeight.bold),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Text(
+                  'First Name & Last Name',
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+                Row(
+                  children: [
+                    Text(
+                      'Loan Number : ',
+                      style: TextStyle(fontSize: 11),
+                    ),
+                    Text(
+                      'xxxxxxxxxxxxxxxxx',
+                      style: TextStyle(fontSize: 11),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 18,
+                ),
+                Text(
+                  'Address',
+                  style: TextStyle(fontSize: 12, fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  '190 tornge car, shionla , Hawai ',
+                  style: TextStyle(fontSize: 18),
+                ),
+                SizedBox(
+                  height: 18,
+                ),
+                Text(
+                  'Contact Number',
+                  style: TextStyle(fontSize: 12, fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  '+9411111111 ',
+                  style: TextStyle(fontSize: 18),
+                ),
+                SizedBox(
+                  height: 18,
+                ),
+                Text(
+                  'Id Number',
+                  style: TextStyle(fontSize: 12, fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  '123456456V ',
+                  style: TextStyle(fontSize: 18),
+                ),
+                SizedBox(
+                  height: 18,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        SizedBox(
+                          height: 18,
+                        ),
+                        Text(
+                          'Branch',
+                          style: TextStyle(
+                              fontSize: 12, fontWeight: FontWeight.bold),
+                        ),
+                        Text(
+                          'Borella',
+                          style: TextStyle(fontSize: 18),
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      width: 60,
+                    ),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        SizedBox(
+                          height: 18,
+                        ),
+                        Text(
+                          'Centre',
+                          style: TextStyle(
+                              fontSize: 12, fontWeight: FontWeight.bold),
+                        ),
+                        Text(
+                          'Kotte Road',
+                          style: TextStyle(fontSize: 18),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+                SizedBox(
+                  height: 18,
+                ),
+                Text(
+                  'Group',
+                  style: TextStyle(fontSize: 12, fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  'Borella ',
+                  style: TextStyle(fontSize: 18),
+                ),
+                SizedBox(
+                  height: 18,
+                ),
+                Text(
+                  'LoanDetails',
+                  style: TextStyle(
+                      color: Colors.blueAccent, fontWeight: FontWeight.bold),
+                ),
+                SizedBox(
+                  height: 18,
+                ),
+                Text(
+                  'Product Name',
+                  style: TextStyle(fontSize: 12, fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  'Product Name HERE',
+                  style: TextStyle(fontSize: 18),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Loan Granted Amount',
+                      style: TextStyle(fontSize: 12),
+                    ),
+                    Text(
+                      '150 000.00',
+                      style: TextStyle(fontSize: 18),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 10,),
+                 Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Owe Amount',
+                      style: TextStyle(fontSize: 12),
+                    ),
+                    Text(
+                      '1000.00',
+                      style: TextStyle(fontSize: 18),
+                    ),
+                  ],
+                ),
+                 SizedBox(height: 10,),
+                 Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Total Outstanding Amount',
+                      style: TextStyle(fontSize: 12),
+                    ),
+                    Text(
+                      '40 000.00',
+                      style: TextStyle(fontSize: 18),
+                    ),
+                  ],
+                ),
+                 SizedBox(height: 10,),
+                 Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'statement',
+                      style: TextStyle(fontSize: 12),
+                    ),
+                    Text(
+                      '1000.00',
+                      style: TextStyle(fontSize: 18),
+                    ),
+                  ],
+                ),
+                 SizedBox(height: 30,),
+               Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'Arreares Days',
+                    style: TextStyle(fontSize: 12),
+                  ),
+                  Text(
+                    '5',
+                    style: TextStyle(fontSize: 18),
+                  ),
+                ],
+              ),
+               SizedBox(height: 10,),
+               Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'Next Collection Date',
+                    style: TextStyle(fontSize: 12),
+                  ),
+                  Text(
+                    '22-AUG-2021',
+                    style: TextStyle(fontSize: 18),
+                  ),
+                ],
+              ),
+                SizedBox(height: 10,),
+               Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'Lorn O--------- Date',
+                    style: TextStyle(fontSize: 12),
+                  ),
+                  Text(
+                    '22-AUG-2019',
+                    style: TextStyle(fontSize: 18),
+                  ),
+                ],
+              ),
+                SizedBox(height: 10,),
+               Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'Loan ---------- Date',
+                    style: TextStyle(fontSize: 12),
+                  ),
+                  Text(
+                    'Last Day of the loan',
+                    style: TextStyle(fontSize: 18),
+                  ),
+                ],
+              ),
+              SizedBox(height: 60,)
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
